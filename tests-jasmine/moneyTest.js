e68d80f8fadc94d7b03d3e254ad0a6d7d95e8b15
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-06-26 17:52:10 +0530

    removed defaults tests and added moneyTest

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+describe('test suite: formatCurrency',()=>{
+    it('converts cents into dollars',()=>{
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+    it('works with 0',()=>{
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+    it('rounding 2000.5 to 20.01',()=>{
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+    it('rounding 2000.4 to 20.00',()=>{
+        expect(formatCurrency(2000.4)).toEqual('20.00');
+    });
+})
