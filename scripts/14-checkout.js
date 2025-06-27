354e36f4c170944d35630af834385f5c2bb608a2
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-06-27 12:29:15 +0530

    created a function to access the cart in cart-oop

--- scripts/14-checkout.js
+++ scripts/14-checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import { renderHeaderSummary } from './checkout/headerSummary.js';
+import '../data/cart-oop.js';
 renderHeaderSummary();
 renderOrderSummary();
 renderPaymentSummary();
