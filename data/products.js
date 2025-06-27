38959437a00b91e54cc36abe7f1142579061be3c
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-06-27 14:43:12 +0530

    cleaner data

--- data/products.js
+++ data/products.js
@@ -19,22 +19,6 @@ class Product{
     return formatCurrency(this.priceCents);
   }
 }
-const p1=new Product({
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
-  });
-console.log(p1);
 
 export const products = [
   {
@@ -698,8 +682,6 @@ export const products = [
 ].map((productDetails)=>{
   return new Product(productDetails);
 });
-
-console.log(products);
 
 export const matchProduct=productID=>{
   return products.filter(product=>product.id===productID)[0];
